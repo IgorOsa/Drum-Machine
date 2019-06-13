acc44c318fd343afdc8fc4c1f23992041e01b227
Igor Osadchyi <igor@webio.net.ua>
2019-06-13 18:12:41 +0300

    Updates

---
+++ precache-manifest.cd49aed67e0902a7dd2590cd8b84ea90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "990f2841877325df73c6903af21a4166",
+    "url": "/Drum-Machine/index.html"
+  },
+  {
+    "revision": "60a577fd0ae5b33b13e1",
+    "url": "/Drum-Machine/static/css/main.bab96bdc.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/Drum-Machine/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "60a577fd0ae5b33b13e1",
+    "url": "/Drum-Machine/static/js/main.df2a4e6c.chunk.js"
+  },
+  {
+    "revision": "69ecda25881826061971",
+    "url": "/Drum-Machine/static/js/runtime~main.2d2b62a3.js"
+  }
+]);
